d94f69016be1c226ae27b63b75bf39d09faf470f
B00mMaster <pablogaudesi@gmail.com>
2022-12-13 13:49:25 +0100

    HECHO
    HECHO

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float speed;
+    public float speed = 20f;
+    public float turnSpeed = 10f;
+    public float horizontalInput;
+    public float verticalInput;
 
-    public float turnSpeed;
 
-    private float horizontalInput;
 
-    private float verticalInput;
-
-    private void Update()
+    void Update()
     {
+        verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
-       
-        verticalInput=Input.GetAxis("Vertical");
-      
-       transform.Translate(Vector3.right * turnSpeed * Time.deltaTime * horizontalInput);
 
         transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
 
-       
+
+        
+
+
+
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
     }
 
-    
+
 
 
 
