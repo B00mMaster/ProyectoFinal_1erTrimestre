cfe5c6db07f54c328fd9c2ab5a46b3dac65ca334
B00mMaster <pablogaudesi@gmail.com>
2022-12-13 18:38:45 +0100

    FINALIZADO
    :D

--- Assets/Scripts/Avion.cs
+++ Assets/Scripts/Avion.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinRotate : MonoBehaviour
+public class Avion : MonoBehaviour
 {
     public float speed = 0.8f;
 
     private void Update()
     {
-        transform.Rotate(0, speed, 0, Space.World);
+        transform.Rotate(0, 0, speed);
     }
 }
